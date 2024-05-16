9d57ccb1a4235b442138a788918e8aed03fd03b8
Edwin Saul <edwinsaulpm@gmail.com>
2024-05-16 13:11:42 -0500

    structure js

--- code/ej005_example_spinner/extra/spinner/spinnerControl.js
+++ code/ej005_example_spinner/extra/spinner/spinnerControl.js
@@ -1,17 +1,21 @@
-// control spinner js
-console.log("spinner");
+// make spinner
+//
+// <div class="overlay" id="overlay">
+//    <div class="spinner"></div>
+// </div>
+//
+//
+//  part 1 -- Making Spinner
+var overlaySpinner=document.getElementById("overlay");
+overlaySpinner.style.distplay="block";
+overlaySpinner.classList.add("overlay");
+var overlaySpinnerDiv= document.createElement("div");
+overlaySpinnerDiv.classList.add("spinner");
+overlaySpinner.appendChild(overlaySpinnerDiv);
 
-// Función para mostrar el spinner
-function showSpinner() {
-  var spinner = document.getElementById('overlay');
-  spinner.style.display = 'block';
-}
-
-// Función para ocultar el spinner
+// Functión to hide spinner
 function hideSpinner() {
   var spinner = document.getElementById('overlay');
   spinner.style.display = 'none';
 }
 
-//hideSpinner();
-//showSpinner();
