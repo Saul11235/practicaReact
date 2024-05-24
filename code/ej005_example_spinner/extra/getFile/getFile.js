1f9a0e50b78a452cdf045e9f1b76450cc0a027f3
Edwin Saul <edwinsaulpm@gmail.com>
2024-05-23 22:51:03 -0500

    structure js cdn

--- code/ej005_example_spinner/extra/getFile/getFile.js
+++ code/ej005_example_spinner/extra/getFile/getFile.js
@@ -1,9 +1,68 @@
 // get File
 //const fetch = require('node-fetch');
 
+var getFile_promise= fetch("");
+
+
 function getFile(url) {
-    return [url,true];
-   }
+  // create new promise
+  getFile_promise=fetch(url);
+  console.log(getFile_promise);
+  //------
+  var textFile="";
+  var isOk=false;
+  var haveData=false;
+  //------
+  getFile_promise.then(
+    response=>{
+      return response.text();
+    }
+  ).then(
+    data=>{
+      isOk=true
+      textFile=""+data;
+      haveData=true
+      console.log(textFile)
+      console.log(typeof textFile)
+      console.log(textFile)
+      console.log(haveData)
+    }
+  ).catch(
+    error=> {
+      isOk=false
+      console.error(error);
+      haveData=true;
+    }
+  )
+  // loop-------------------------
+  function makeTime() { return new Promise(resolve => setTimeout(resolve,5))};
+
+  function loopTime() {
+    let contador=0;
+    function viewData() {
+      console.log("bucle",contador);
+      contador+=1;
+      if (!haveData) {
+	makeTime().then(viewData)
+      };
+    };
+    viewData();
+  }
+  //------------------------------
+  loopTime();
+  //------------------------------
+  //------------------------------
+  // infinite loop
+  let response =[]
+  // return ------------------------
+    if (isOk) { response= await [textFile,true];
+    } else { response=[url,false];
+    };
+  console.log(response);
+  //--------------------------------
+  return response;
+
+   };
     
 
 // get content of file
